c14bc7a77917c0dbdbde3670854a4029572dea29
Apaar Garg <apaar.garg@freshworks.com>
2022-09-20 20:34:32 +0530

    Sample_App with version 3.2.22.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp32225::Application.config.secret_token = '3e679ab39b59d31bcc97b944f40586876e000386dbe813463e758dcd0936d1eee2552f03ecb66b9c1194da0d3a24aeff044a4236e6aa8b59e328b8210f5cc79b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SampleApp32225::Application.config.session_store :cookie_store, key: '_Sample_App_3.2.22.5_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SampleApp32225::Application.config.session_store :active_record_store
