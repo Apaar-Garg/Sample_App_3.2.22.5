6472dd6db4cf6dfa48858ae18d23a886dbdbfbfa
Apaar Garg <apaar.garg@freshworks.com>
2022-09-26 13:21:27 +0530

    Hello World printing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  #protect_from_forgery
+  def hello
+    render text: "hello world!", content_type: 'text/plain'
+  end
 end
