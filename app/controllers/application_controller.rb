977613eef822eb991c818303a98cb7077adb5d53
Apaar Garg <apaar.garg@freshworks.com>
2022-09-26 16:15:48 +0530

    WELCOME HEADING

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery
   def hello
-    render text: "hello world!", content_type: 'text/plain'
+    render text: "WELCOME", content_type: 'text/plain'
   end
 end
